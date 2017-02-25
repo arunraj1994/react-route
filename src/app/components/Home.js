252341a8fde1d79f6cd9a2e80407496180251faf
arunraj1994 <arunrajbe94@gmail.com>
2017-02-25 17:48:06 +0530

    Add files via upload

---
+++ src/app/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export class Home extends React.Component {
+	render() {
+		// console.log(this);
+		return (
+			<div>
+				<h3>Home</h3>
+				<div class="jumbotron">Hello</div>
+			</div>
+		)
+	}
+}
